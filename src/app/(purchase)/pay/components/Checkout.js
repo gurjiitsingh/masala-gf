38d9af6b4b877b6b5918f972854d4450e918256b
gurnit <gurjiitsingh@gmail.com>
2025-04-25 10:36:24 +0530

    paypal debug

--- src/app/(purchase)/pay/components/Checkout.js
+++ src/app/(purchase)/pay/components/Checkout.js
@@ -4,6 +4,8 @@ import {
    
     usePayPalScriptReducer,
   } from "@paypal/react-paypal-js";
+
+  
   
   import { useRouter, useSearchParams } from "next/navigation";
   import { useCartContext } from "@/store/CartContext";
@@ -11,24 +13,15 @@ import {
 export default function Checkout(){
  const searchParams = useSearchParams();
    const orderMasterId = searchParams.get("orderMasterId");
-  // console.log("orderMasterId in paypal -------------",orderMasterId)
+  
 
     const [{ options, isPending,isRejected,isResolved, isInitial  }, dispatch] = usePayPalScriptReducer();
-    //const [currency, setCurrency] = useState(options.currency);
-  //  console.log("isPending,isRejected,isResolved, isInitial ---------", isPending,isRejected,isResolved, isInitial)
+  
     const router = useRouter();
     const {   endTotalG } = useCartContext();
     const formattedAmount = (Number(endTotalG) || 0).toFixed(2);
 
    
-    //  useEffect(()=>{
-    //   console.log("endTotalG , type --------",endTotalG,typeof(endTotalG));
-    // })
-
-    // let customerAddress;
-    // if (typeof window !== 'undefined') {
-    //  customerAddress = JSON.parse(localStorage.getItem("customer_address") || '""')  ;
-    // }
 
     let customerAddress = {};
     if (typeof window !== 'undefined') {
@@ -36,51 +29,27 @@ export default function Checkout(){
       customerAddress = address ? JSON.parse(address) : {};
     }
 
-// Validate German postal code (5 digits)
-// const validatePostalCode = (postalCode) => {
-//   const postalCodePattern = /^\d{5}$/; // German postal codes are always 5 digits
-//   return postalCodePattern.test(postalCode);
-// };
+  
+     const onCreateOrder = (data,actions) => {
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: formattedAmount,
+                    },
+                },
+            ],
+        });
+    }
+  
+  
+  
 
-    // if (customerAddress?.postalCode && !validatePostalCode(customerAddress?.postalCode)) {
-    //   alert("Please enter a valid German postal code.");
-    //   return; // Stop the order creation if postal code is invalid
-    // }
-  //console.log("cartData ", productTotalCost)
-  const onCreateOrder = (data, actions) => {
-    return actions.order.create({
-      intent: "CAPTURE",
-      application_context: {
-        shipping_preference: "NO_SHIPPING",
-       // user_action: "PAY_NOW"
-      },
-      purchase_units: [
-        {
-          amount: {
-            value: formattedAmount, // should be string like "10.00"
-            currency_code: "EUR",
-          },
-        },
-      ],
-      payer: {
-        name: {
-          given_name: customerAddress?.firstName || '',
-          surname: customerAddress?.lastName || '',
-        },
-        email_address: customerAddress?.email || '',
-        address: {
-          address_line_1: customerAddress?.addressLine1 || '',
-          address_line_2: customerAddress?.addressLine2 || '',
-          admin_area_2: customerAddress?.city || '',        // City
-          admin_area_1: customerAddress?.state || '',       // State
-          postal_code: customerAddress?.zipCode || '',      // Postal Code
-          country_code: "DE",                               // Germany
-        },
-      },
-    });
-  };
+  
     const onApproveOrder = (data, actions) => {
       return actions.order.capture().then((details) => {
+	  // const payer = details.payer;
+    // 	  console.log("details-----",payer);
           router.push(`/complete?paymentType=paypal&status=success&orderMasterId=${orderMasterId}`)
        });
     };
@@ -100,14 +69,16 @@ export default function Checkout(){
   
     return (<div className="flex container mx-auto px-[30%] items-center justify-center my-[20%] ">
       <div className="checkout">
-        {(isPending || typeof window === 'undefined' || !window.paypal) ? (
-          <p>LOADING...</p>
-        ) : (
+      {isPending && <p>Loading PayPal...</p>}
+
+{isRejected && <p>Failed to load PayPal. Please refresh or try again later.</p>}
+
+{isResolved && (
           <>
         
             <PayPalButtons
               message={{
-                amount: endTotalG,
+                amount: formattedAmount,
                 align: "center",
                 color: "black",
                 position: "top",
@@ -118,6 +89,8 @@ export default function Checkout(){
               onError={ (err)=> onError(err)}
               onCancel = {(data)=> onCancel(data)}
             />
+
+            
            
           </>
         )}
