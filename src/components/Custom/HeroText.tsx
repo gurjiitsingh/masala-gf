dbd6635ef3077e76ffcb544d1568349136646ecb
gurnit <gurjiitsingh@gmail.com>
2025-06-01 09:41:38 +0530

    hero text

--- src/components/Custom/HeroText.tsx
+++ src/components/Custom/HeroText.tsx
@@ -8,8 +8,10 @@ const bebas = Bebas_Neue({
 export default function HeroText() {
   return (
     <div>
-         <h2 className={`${bebas.className} text-3xl w-full my-2 text-emerald-900`} >Abhol Rabatt 20%</h2>
-         <div className='p-2 bg-amber-200 text-sky-600 font-semibold rounded-xl mb-2'>Lieferung ist ebenfalls verfügbar.</div>
+      <div className='flex mt-2 md:mt-0 md:flex-col gap-2'>
+         <h2 className={`${bebas.className} text-3xl w-fit my-2 text-emerald-900 `} >Abhol Rabatt 20%</h2>
+         <div className='p-2 bg-amber-200 text-sky-600 w-fit h-fit font-semibold rounded-xl mb-2'>Lieferung ist ebenfalls verfügbar.</div>
+         </div>
                   <p className="text-sm text-slate-500">Das Essen kann vom Bild abweichen</p>
     </div>
   )
