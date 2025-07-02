0507efc33cb923076a6ff53ff2ed7273af030544
gurnit <gurjiitsingh@gmail.com>
2025-07-02 11:48:55 +0530

    update

--- src/app/(universal)/admin/orders/order-detail/components/OrderDetail.tsx
+++ src/app/(universal)/admin/orders/order-detail/components/OrderDetail.tsx
@@ -23,7 +23,7 @@ const OrderDetail = () => {
   const masterOrderId = searchParams.get("masterId") as string;
   const [orderProducts, setOrderProducts] = useState<orderProductsT[]>([]);
   const [customerAddress, setCustomerAddress] = useState<addressResT>();
-  const [orderMasterData, setOrderMasterData] = useState<orderMasterDataT>();
+  const [orderMasterData, setOrderMasterData] = useState<orderMasterDataT | null>(null);
 
   useEffect(() => {
     async function getOrderProducts() {
