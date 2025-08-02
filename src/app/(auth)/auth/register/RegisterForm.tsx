6974e31badfb23d624cc742c86b3c2e1b7933d8a
gurnit <gurjiitsingh@gmail.com>
2025-08-02 14:17:13 +0530

    update

--- src/app/(auth)/auth/register/RegisterForm.tsx
+++ src/app/(auth)/auth/register/RegisterForm.tsx
@@ -97,7 +97,7 @@ export default function RegisterForm() {
 
       <p className="text-sm text-center mt-6">
         Already have an account?{" "}
-        <a href="/login" className="text-blue-600 hover:underline">
+        <a href="/auth/login" className="text-blue-600 hover:underline">
           Login here
         </a>
       </p>
